64d9dcb2be2ab6d3a6285c0268f7a6cfeb1de886
werner <nineofnine@gmail.com>
2021-03-03 09:39:53 +1100

    latest

--- src/data/sets.py
+++ src/data/sets.py
@@ -134,3 +134,44 @@ def load_sets(path='../data/processed/', val=False):
     y_test  = np.load(f'{path}y_test.npy' ) if os.path.isfile(f'{path}y_test.npy')  else None
     
     return X_train, y_train, X_val, y_val, X_test, y_test
+    
+    
+    
+def split_sets_random(df, target_col, test_ratio=0.2, to_numpy=False):
+    """Split sets randomly
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe
+    target_col : str
+        Name of the target column
+    test_ratio : float
+        Ratio used for the validation and testing sets (default: 0.2)
+
+    Returns
+    -------
+    Numpy Array
+        Features for the training set
+    Numpy Array
+        Target for the training set
+    Numpy Array
+        Features for the validation set
+    Numpy Array
+        Target for the validation set
+    Numpy Array
+        Features for the testing set
+    Numpy Array
+        Target for the testing set
+    """
+    
+    from sklearn.model_selection import train_test_split
+    
+    features, target = pop_target(df=df, target_col=target_col, to_numpy=to_numpy)
+    
+    X_data, X_test, y_data, y_test = train_test_split(features, target, test_size=test_ratio, random_state=8)
+    
+    val_ratio = test_ratio / (1 - test_ratio)
+    X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=val_ratio, random_state=8)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
